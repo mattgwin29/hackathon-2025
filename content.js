36a734c0fac3715ff870c3335a5baf412070ef86
Andrew Roberts <andrew@azroberts.com>
2025-03-02 08:11:57 -0500

    Automatically get inputted password on blur

--- content.js
+++ content.js
@@ -5,3 +5,25 @@
 // Project Repository URI: https://github.com/
 // Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
 // License: MIT
+
+const pwdInput = getPwdInput();
+
+function getPwdInput() {
+    const allInputs = document.getElementsByTagName("input");
+    
+    let pwdInput;
+    for(let i = 0; i < allInputs.length; i++) {
+        if(allInputs[i].type.toLowerCase() === "password") {
+            pwdInput = allInputs[i];
+            break;
+        }
+    }
+
+    return pwdInput;
+}
+
+
+
+getPwdInput().addEventListener("blur", e => {
+    console.log(e.currentTarget.value);
+});
