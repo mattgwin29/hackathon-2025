cc19e2b295d25f340bf924cfb42ebfab84eb0a2c
darwashiOM <darwashi@udel.edu>
2025-03-02 09:28:29 -0500

    auto filling password

--- checkOrCreate.js
+++ checkOrCreate.js
@@ -3,23 +3,34 @@ import { ref, get } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-dat
 import { hashPassword } from "./hashing.js";
 import { hashpw } from "./bcrypt.js"
 
-async function checkOrCreateSaltPepper(userId, domain, plainPassword) {
-  domain = domain.replaceAll(".", "_")
-  const pathRef = ref(database, `users/${userId}/${domain}`);
-  
-  get(pathRef).then(async (snapshot) => {
-    if (snapshot.exists()) {
+export async function checkOrCreateSaltPepper(userId, domain, plainPassword) {
+  // Return a Promise so we can retrieve the final substring outside
+  return new Promise((resolve, reject) => {
+    domain = domain.replaceAll(".", "_");
+    const pathRef = ref(database, `users/${userId}/${domain}`);
+    
+    get(pathRef)
+      .then(async (snapshot) => {
+        if (snapshot.exists()) {
+          const { salt, pepper } = snapshot.val();
+          console.log(salt, pepper);
 
-      const { salt, pepper } = snapshot.val();
-      console.log(salt, pepper);
-      hashpw(pepper + plainPassword, salt, async hash => {
-        console.log(hash)
-        document.getElementById("output").innerText = `Your 3FA password: ${hash.substring(45, 60)}`;
-            }, undefined);
-    } else {
-      await hashPassword(userId, domain, plainPassword);
-    }
+          hashpw(pepper + plainPassword, salt, async (hash) => {
+            
+
+            const finalPassword = hash.substring(45, 60);
+            document.getElementById("output").innerText = `Your 3FA password: ${finalPassword}`;
+            resolve(finalPassword);
+          });
+        } else {
+          const finalPassword = await hashPassword(userId, domain, plainPassword);
+          document.getElementById("output").innerText = `Your 3FA password: ${finalPassword}`;
+            resolve(finalPassword);
+        }
+      })
+      .catch((err) => {
+        console.error("Error in checkOrCreateSaltPepper:", err);
+        reject(err);
+      });
   });
 }
-
-export { checkOrCreateSaltPepper };
